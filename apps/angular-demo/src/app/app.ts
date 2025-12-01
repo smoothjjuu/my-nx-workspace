4283a67b35735dc9c9af46a50c035ae2f6f3aaf2
jjuu <78110371+smoothjjuu@users.noreply.github.com>
2025-12-01 11:29:55 +0000

    add hero

--- apps/angular-demo/src/app/app.ts
+++ apps/angular-demo/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+// importing the component from the library
+import { Hero } from '@angular-demo/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, NxWelcome, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
