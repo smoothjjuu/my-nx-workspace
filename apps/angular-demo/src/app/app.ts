69a73e7ff4abc7c4c4c5f65b08229c1eaddaadf5
jjuu <78110371+smoothjjuu@users.noreply.github.com>
2025-12-01 11:35:23 +0000

    fix issue

--- apps/angular-demo/src/app/app.ts
+++ apps/angular-demo/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 // importing the component from the library
 import { Hero } from '@angular-demo/ui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NxWelcome, Hero],
+  imports: [RouterModule, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
