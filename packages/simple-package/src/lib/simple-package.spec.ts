ea999e07813acb1d749014afbad7e2fac297a69f
jjuu <78110371+smoothjjuu@users.noreply.github.com>
2025-12-01 12:37:58 +0000

    create simple pkg

---
+++ packages/simple-package/src/lib/simple-package.spec.ts
@@ -0,0 +1,7 @@
+import { simplePackage } from './simple-package';
+
+describe('simplePackage', () => {
+  it('should work', () => {
+    expect(simplePackage()).toEqual('simple-package');
+  });
+});
